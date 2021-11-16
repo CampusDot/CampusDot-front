1d09c544f700958ec8e61b41b8d30691d33da235
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-16 22:52:27 +0900

    feat: Add Home related screens

---
+++ src/screens/Main/Home/SelectedList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import SelectedList from 'templates/Main/Home/SelectedList';
+
+export default function () {
+  return <SelectedList />;
+}
---
+++ src/screens/Main/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Home from 'templates/Main/Home';
+
+export default function () {
+  return <Home />;
+}
