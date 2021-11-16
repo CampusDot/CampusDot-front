7c1bc9d17a541725bda2cd24475bf2157d8edd63
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-16 23:02:46 +0900

    fix: path changes

--- src/navigation/Main/Tab.js
+++ src/navigation/Main/Tab.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from 'screens/Main/Main';
+import Home from 'screens/Main/Home';
 
 const Tab = createBottomTabNavigator();
 
