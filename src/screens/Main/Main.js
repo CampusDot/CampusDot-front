466aad2cae957851f9f88e86c1bb74ed1d457947
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-16 14:24:12 +0900

    feat: Add Home templates to Main components

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Home from 'templates/Main/Home';
 
 const Main = () => {
-  return <Text>Main</Text>;
+  return <Home />;
 };
 
 export default Main;
