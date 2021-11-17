78e3a95db73894455f67fb0ab9fd383f89c7ceba
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-17 13:13:48 +0900

    fix: add Provider in App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import { Provider as AuthProvider } from 'context/Auth';
 import MainNavigator from './src/navigation';
+import { Provider as UserProvider } from './src/context/User';
 
 export default () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,8 +15,10 @@ export default () => {
   };
 
   return (
-    <AuthProvider>
-      <MainNavigator />
-    </AuthProvider>
+    <UserProvider>
+      <AuthProvider>
+        <MainNavigator />
+      </AuthProvider>
+    </UserProvider>
   );
 };
