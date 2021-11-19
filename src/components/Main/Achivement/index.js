b80ef584b4e0205de6a68a5ac44e384a39191817
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-19 20:27:19 +0900

    feat: Add related with achivements components and add in Home templates

---
+++ src/components/Main/Achivement/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import style from 'constants/styles';
+import { getStamp, getNextStamp } from 'lib/utils/achivements';
+import Level from './Level';
+import ProgressBar from './ProgressBar';
+
+const Achivement = () => {
+  const stamp = 13;
+  return (
+    <View style={{ marginTop: 50 }}>
+      <View style={[style.space_between, style.flexRow]}>
+        <Level stamp={stamp} />
+        <Level stamp={stamp} goal />
+      </View>
+      {getNextStamp(stamp) !== getStamp(stamp) && (
+        <ProgressBar step={stamp - getStamp(stamp)} steps={getNextStamp(stamp) - getStamp(stamp)} />
+      )}
+    </View>
+  );
+};
+
+export default Achivement;
--- src/templates/Main/Home/index.js
+++ src/templates/Main/Home/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import Challenge from 'components/Main/Challenge';
 import RestaurantLists from 'components/Main/RestaurantLists';
+import Achivement from 'components/Main/Achivement';
 
 const Home = () => {
   return (
     <ScrollView>
+      <Achivement />
       <Challenge />
       <RestaurantLists />
     </ScrollView>
