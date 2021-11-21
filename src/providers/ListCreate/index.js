f0c91b897cc0c4056dcdbbf88024d324f72e2dcf
Hyodori04 <x40807@gmail.com>
2021-11-21 22:49:56 +0900

    feat: make provider for listcreate

---
+++ src/providers/ListCreate/index.js
@@ -0,0 +1,64 @@
+import React, { createContext, useContext, useState, useRef } from 'react';
+
+const ListCreateContext = createContext(null);
+export const useListCreate = () => useContext(ListCreateContext);
+
+const ListCreateProvider = ({ children }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [commenting, setCommenting] = useState(false);
+  const [currentStore, setCurrentStore] = useState();
+  const [currentComment, setCurrentComment] = useState();
+  const [stores, setStores] = useState([]);
+  const [edit, setEdit] = useState(false);
+
+  const informationRef = useRef({
+    title: '',
+    content: '',
+  });
+  const onAddStore = () => {
+    setStores([...stores, { store: currentStore, comment: currentComment }]);
+  };
+  const onEditStore = () => {
+    // dont know how eo edit
+    setEdit(false);
+  };
+
+  const onClickDeleteSong = (object) => {
+    setStores(stores.filter((item) => item.store._id !== object.store._id));
+  };
+
+  const onClickEdit = (object) => {
+    setCurrentStore(object.store);
+    setCurrentComment(object.comment);
+    setCommenting(true);
+    setEdit(true);
+  };
+
+  // eslint-disable-next-line react/jsx-no-constructed-context-values
+  const value = {
+    informationRef,
+    title,
+    content,
+    commenting,
+    currentStore,
+    currentComment,
+    stores,
+    edit,
+    setEdit,
+    setCommenting,
+    setTitle,
+    setContent,
+    setCurrentStore,
+    setCurrentComment,
+    setStores,
+    onAddStore,
+    onEditStore,
+    onClickDeleteSong,
+    onClickEdit,
+  };
+
+  return <ListCreateContext.Provider value={value}>{children}</ListCreateContext.Provider>;
+};
+
+export default ListCreateProvider;
