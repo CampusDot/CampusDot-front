98affe63c3a8a5bef98500014494fdfe36e9e2b5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-21 18:47:59 +0900

    fix: Add Header in Home

--- src/templates/Main/Home/index.js
+++ src/templates/Main/Home/index.js
@@ -3,14 +3,17 @@ import { ScrollView } from 'react-native';
 import Challenge from 'components/Main/Challenge';
 import RestaurantLists from 'components/Main/RestaurantLists';
 import Achivement from 'components/Main/Achivement';
+import Header from 'components/Header';
 
 const Home = () => {
   return (
     <ScrollView>
+      <Header title="연세대학교" back />
       <Achivement />
       <Challenge />
       <RestaurantLists />
     </ScrollView>
   );
 };
+
 export default Home;
