266b702928331a15857c47cbd166b827501bc450
Hyodori04 <x40807@gmail.com>
2021-11-21 22:49:26 +0900

    feat: make components for storelist comment and title

---
+++ src/components/Main/ListInput/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Pressable, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import TextsInput from './TextsInput';
+
+const ListInput = () => {
+  return (
+    <View>
+      <TextsInput />
+    </View>
+  );
+};
+
+export default ListInput;
