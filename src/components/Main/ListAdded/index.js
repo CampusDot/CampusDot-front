a0416f8a402198c4b10f38518ec9abb6fff084be
Hyodori04 <x40807@gmail.com>
2021-11-22 01:26:46 +0900

    fix : function form changed, get object parameter with new name

--- src/components/Main/ListAdded/index.js
+++ src/components/Main/ListAdded/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { useListCreate } from 'providers/ListCreate';
-import style from '../../../constants/styles';
+import style from 'constants/styles';
 
 const ListAdded = ({ setIsSearch }) => {
-  const { stores, onClickDeleteSong, onClickEdit } = useListCreate();
-  const onEdit = async (item) => {
+  const { stores, onClickDeleteStore, onClickEdit } = useListCreate();
+  const onPressEdit = (item) => {
     setIsSearch(true);
     onClickEdit(item);
   };
+
   return (
     <View>
       <FlatList
@@ -17,27 +18,20 @@ const ListAdded = ({ setIsSearch }) => {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.padding}
         renderItem={({ item }) => {
+          const { store, comment } = item;
           return (
             <View style={style.flexRow}>
               <View style={styles.image} />
               <View>
-                <Text>{item.store.Information.name}</Text>
-                <Text>{item.store.vicinity}</Text>
-                <Text>{item.comment}</Text>
+                <Text>{store.Information.name}</Text>
+                <Text>{store.vicinity}</Text>
+                <Text>{comment}</Text>
               </View>
               <View>
-                <TouchableOpacity
-                  onPress={() => {
-                    onEdit(item);
-                  }}
-                >
+                <TouchableOpacity onPress={() => onPressEdit(item)}>
                   <Text>수정하기</Text>
                 </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => {
-                    onClickDeleteSong(item);
-                  }}
-                >
+                <TouchableOpacity onPress={() => onClickDeleteStore(item)}>
                   <Text>지우기</Text>
                 </TouchableOpacity>
               </View>
