dd5907b2fe15d3670ca3c6baeb9aa5c2b09d8aad
Hyodori04 <x40807@gmail.com>
2021-11-21 22:49:42 +0900

    feat: make components for reviewinput

---
+++ src/components/Main/ReviewInput/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Pressable, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { goBack, navigate } from 'lib/utils/navigation';
+
+const ReviewInput = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>ReviewInput</Text>
+    </View>
+  );
+};
+
+export default ReviewInput;
