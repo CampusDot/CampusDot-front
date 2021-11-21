0410f5b7be191a925d9404f640334c5a8dca744f
Hyodori04 <x40807@gmail.com>
2021-11-21 22:47:58 +0900

    feat: make components for creat list header

---
+++ src/components/Main/CreatListHeader/index.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import style from 'constants/styles';
+import { goBack } from 'lib/utils/navigation';
+import { Context } from 'context/StoreList';
+import { useListCreate } from 'providers/ListCreate';
+
+const Header = () => {
+  // const { Name } = PostUser;
+  const { postStoreList } = useContext(Context);
+  const { stores, informationRef } = useListCreate();
+
+  return (
+    <View style={[styles.container, style.flexRow]}>
+      <TouchableOpacity
+        onPress={() => {
+          goBack();
+        }}
+      >
+        <Text>뒤로가기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          postStoreList({
+            Stores: stores,
+            Title: informationRef.title,
+            Comment: informationRef.content,
+          });
+        }}
+      >
+        <Text>업로드하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    borderWidth: 1,
+    justifyContent: 'space-between',
+  },
+  profileImg: {
+    width: 24,
+    height: 24,
+    borderRadius: 24,
+    borderWidth: 1,
+  },
+});
+
+export default Header;
