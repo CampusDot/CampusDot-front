6e6840d8f6812df8bf2ad4f4e0d703109b86fd69
Hyodori04 <x40807@gmail.com>
2021-11-22 01:27:00 +0900

    fix : <View> removed

--- src/components/Main/ListInput/index.js
+++ src/components/Main/ListInput/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { View, Pressable, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import TextsInput from './TextsInput';
 
 const ListInput = () => {
-  return (
-    <View>
-      <TextsInput />
-    </View>
-  );
+  return <TextsInput />;
 };
 
 export default ListInput;
