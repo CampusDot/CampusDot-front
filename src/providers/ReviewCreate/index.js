d7540185bcc41aadb0258d0c26b87692c6316a0f
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-25 02:19:09 +0900

    refactor: refactor code with reviewCreate providers

--- src/providers/ReviewCreate/index.js
+++ src/providers/ReviewCreate/index.js
@@ -1,26 +1,56 @@
 import React, { createContext, useContext, useState } from 'react';
+import { Context as ReviewContext } from 'context/Review';
+import { navigate } from 'lib/utils/navigation';
 
 const ReviewCreateContext = createContext(null);
+
 export const useReviewCreate = () => useContext(ReviewCreateContext);
 
 const ReviewCreateProvider = ({ children }) => {
-  const [review, setReview] = useState('');
-  const [rating, setRating] = useState('0');
+  const { postReview } = useContext(ReviewContext);
   const [image, setImage] = useState([]);
-  const [store, setStore] = useState(null);
-  const [edit, setEdit] = useState(false);
+  const [information, setInformation] = useState({
+    review: '',
+    rating: 0,
+    image: [],
+    storeId: null,
+  });
+
+  const onChangeValue = (type, value) => {
+    if (type === 'rating') {
+      setInformation({
+        ...information,
+        rating: value,
+      });
+    } else if (type === 'review') {
+      setInformation({
+        ...information,
+        review: value,
+      });
+    } else if (type === 'store') {
+      setInformation({
+        ...information,
+        storeId: value,
+      });
+    }
+  };
+
+  const onClickPostReview = async () => {
+    navigate('Home');
+    await postReview({
+      Content: information.review,
+      Rating: information.rating,
+      Store: information.storeId,
+    });
+  };
 
   const value = {
-    review,
-    rating,
-    store,
-    edit,
+    information,
     image,
-    setReview,
-    setRating,
-    setStore,
-    setEdit,
     setImage,
+    setInformation,
+    onChangeValue,
+    onClickPostReview,
   };
 
   return <ReviewCreateContext.Provider value={value}>{children}</ReviewCreateContext.Provider>;
