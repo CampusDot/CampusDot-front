c3d81e110f11dea92b29bc9553961f6ec4573af3
Hyodori04 <x40807@gmail.com>
2021-11-24 12:25:19 +0900

    fix : merge header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { goBack } from 'lib/utils/navigation';
 import style from 'constants/styles';
+import { Context as StoreListContext} from 'context/StoreList';
+import { Context as ReviewContext } from 'context/Review';
+import { useListCreate } from 'providers/ListCreate';
+import {useReviewCreate} from 'providers/ReviewCreate';
 
 const Header = ({ title, titleStyle, back, landings, actions }) => {
   const onClickBack = () => {
@@ -24,6 +28,100 @@ const Header = ({ title, titleStyle, back, landings, actions }) => {
   );
 };
 
+export const  CreatListHeader = () => {
+  const { postStoreList } = useContext(StoreListContext);
+  const { stores, informationRef } = useListCreate();
+  const onClickPostStoreList = () => {
+    postStoreList({
+      Stores: stores,
+      Title: informationRef.title,
+      Comment: informationRef.content,
+    });
+  };
+
+  return (
+    <View style={[styles.CLHcontainer, style.flexRow]}>
+      <TouchableOpacity onPress={goBack}>
+        <Text>뒤로가기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onClickPostStoreList}>
+        <Text>업로드하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
+export const CreateListSearchHeader = ({ setIsSearch }) => {
+  const [text, setText] = useState();
+  const onClickBack = () => {
+    setIsSearch(false);
+  };
+  return (
+    <View style={[styles.CLSHcontainer, style.flexRow]}>
+      <TouchableOpacity onPress={onClickBack}>
+        <Text>뒤로가기</Text>
+      </TouchableOpacity>
+      <TextInput
+        value={text}
+        onChangeText={(txt) => setText(txt)}
+        placeholder="가게를 검색해주세요."
+        multiline = {false}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+};
+
+export const SelctedStoreHeader = () => {
+  const onClickBack = () => {
+    goBack();
+  };
+  return (
+    <View style={[styles.CLSHcontainer, style.flexRow]}>
+      <TouchableOpacity onPress={onClickBack}>
+        <Text>뒤로가기</Text>
+      </TouchableOpacity>
+      <Text>리뷰</Text>
+    </View>
+  );
+};
+
+export const ReviewWriteHeader = ({ setIsWrite }) => {
+  const { setStore, store, setReview, review, setRating, rating} = useReviewCreate();
+  const { postReview } = useContext(ReviewContext);
+
+  const onClickBack = () => {
+    goBack();
+  };
+
+  const onClickComplete = async() => {
+    await postReview({
+      Content: review,
+      Rating: rating,
+      Store: store
+    })
+    setStore(null);
+    setReview('');
+    setRating('0');
+    setIsWrite(false);
+  };
+
+  return (
+    <View style={[styles.CLSHcontainer, style.flexRow]}>
+      <TouchableOpacity onPress={onClickBack}>
+        <Text>뒤로가기       </Text>
+      </TouchableOpacity>
+      <Text>리뷰 작성        </Text>
+      <TouchableOpacity onPress={onClickComplete}>
+        <Text>완료</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
 const styles = StyleSheet.create({
   container: {
     height: 40,
@@ -42,6 +140,21 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 15,
     zIndex: 99,
+  },
+  CLHcontainer: {
+    height: 40,
+    borderWidth: 1,
+    justifyContent: 'space-between',
+  },
+  CLSHcontainer: {
+    height: 40,
+    borderWidth: 1,
+  },
+  profileImg: {
+    width: 24,
+    height: 24,
+    borderRadius: 24,
+    borderWidth: 1,
   },
 });
 
