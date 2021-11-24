b90810dafc08ee1abe0d3d49b1afd4434cdd2063
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-24 20:08:11 +0900

    fix: code refactor in mypage

--- src/screens/Main/MyPage/index.js
+++ src/screens/Main/MyPage/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import MyPage from 'templates/Main/MyPage';
-import { Provider } from 'context/College';
 
 export default function () {
-  return (
-    <Provider>
-      <MyPage />
-    </Provider>
-  );
+  return <MyPage />;
 }
--- src/templates/Main/MyPage/index.js
+++ src/templates/Main/MyPage/index.js
@@ -1,39 +1,16 @@
-import React, { useContext, useState } from 'react';
-import { Button, View } from 'react-native';
-import { Context as CollegeContext } from 'context/College';
+import React from 'react';
+import { View } from 'react-native';
+import Profile from 'components/MyPage/Profile';
+import Section from 'components/MyPage/Section';
 
 const MyPage = () => {
-  const [category, setCategory] = useState('All');
-  const { getStore, state, getStoreLists, initStore, getOtherCollegeRanking, getMyCollegeRanking } =
-    useContext(CollegeContext);
-
-  const onClickCategory = (option) => {
-    initStore();
-    setCategory(option);
-    getStore({ category: option, page: 0 });
-  };
-
-  const nextCategory = () => {
-    getStore({ category, page: state.storePage });
-  };
-
   return (
     <View style={{ marginTop: 100 }}>
-      <Button title="전체" onPress={() => onClickCategory('All')} />
-      <Button title="한식" onPress={() => onClickCategory('Korean')} />
-      <Button title="중식" onPress={() => onClickCategory('Chinese')} />
-      <Button title="일식" onPress={() => onClickCategory('Japanese')} />
-      <Button title="양식" onPress={() => onClickCategory('Western')} />
-      <Button
-        title="get CollegeStore"
-        onPress={() => getStore({ category, page: state.storePage })}
-      />
-      <Button
-        title="get CollegeStoreLists"
-        onPress={() => getStoreLists({ page: state.storeListsPage })}
-      />
-      <Button title="우리학교 랭킹" onPress={getMyCollegeRanking} />
-      <Button title="다른학교 랭킹" onPress={getOtherCollegeRanking} />
+      <Profile />
+      <Section title="먹어 볼래" />
+      <Section title="내가 쓴 리스트" />
+      <Section title="다녀 온 가게" />
+      <Section title="설정" />
     </View>
   );
 };
