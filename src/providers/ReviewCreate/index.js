3ed63feb7ab788460f3de3c5ad457882184e26cb
Hyodori04 <x40807@gmail.com>
2021-11-24 12:54:41 +0900

    feat: Review Provider

---
+++ src/providers/ReviewCreate/index.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ReviewCreateContext = createContext(null);
+export const useReviewCreate = () => useContext(ReviewCreateContext);
+
+const ReviewCreateProvider = ({ children }) => {
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState('0');
+  const [image, setImage] = useState([]);
+  const [store, setStore] = useState(null);
+  const [edit, setEdit] = useState(false);
+
+  // eslint-disable-next-line react/jsx-no-constructed-context-values
+  const value = {
+    review,
+    rating,
+    store,
+    edit,
+    image,
+    setReview,
+    setRating,
+    setStore,
+    setEdit,
+    setImage,
+  };
+
+  return <ReviewCreateContext.Provider value={value}>{children}</ReviewCreateContext.Provider>;
+};
+
+export default ReviewCreateProvider;
