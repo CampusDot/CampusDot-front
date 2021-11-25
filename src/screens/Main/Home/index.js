d9613f73b4aafb23c47da38d54dc054fd39f578a
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-25 14:19:11 +0900

    fix: Add challengeLists with real data and refactor with these

--- src/screens/Main/Home/index.js
+++ src/screens/Main/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from 'templates/Main/Home';
 import { Provider as CollegeProvider } from 'context/College';
+import { Provider as StoreListProvider } from 'context/StoreList';
 
 export default function () {
   return (
     <CollegeProvider>
-      <Home />
+      <StoreListProvider>
+        <Home />
+      </StoreListProvider>
     </CollegeProvider>
   );
 }
