75b7f5429184532a7a0e342840fde4cb58643296
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-26 15:32:27 +0900

    fix: change layout in postUser Header

--- src/components/PostUserHeader/index.js
+++ src/components/PostUserHeader/index.js
@@ -3,28 +3,47 @@ import { Text, View, StyleSheet } from 'react-native';
 import ProfileImage from 'widgets/ProfileImage';
 import style from 'constants/styles';
 import getAchivement from 'lib/utils/achivements';
+import FS, { SCALE_HEIGHT, SCALE_WIDTH } from 'lib/utils/normalize';
 
 const Header = ({ PostUser }) => {
   const { Name, ProfileImage: profileImg, AllStamp } = PostUser;
   return (
     <View style={[styles.container, style.flexRow]}>
       <ProfileImage imageStyle={styles.profileImg} image={profileImg} />
-      <Text>{Name}</Text>
-      <Text>{getAchivement(AllStamp)}</Text>
+      <Text style={styles.name}>{Name}</Text>
+      <View style={styles.box}>
+        <Text style={styles.achivement}>{getAchivement(AllStamp)}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 40,
-    borderWidth: 1,
+    height: 40 * SCALE_HEIGHT,
   },
   profileImg: {
-    width: 24,
-    height: 24,
-    borderRadius: 24,
+    width: 24 * SCALE_WIDTH,
+    height: 24 * SCALE_WIDTH,
+    borderRadius: 24 * SCALE_WIDTH,
     borderWidth: 1,
+    marginLeft: 12 * SCALE_WIDTH,
+  },
+  name: {
+    fontSize: FS(12),
+    marginLeft: 6 * SCALE_WIDTH,
+    marginRight: 4 * SCALE_WIDTH,
+  },
+  box: {
+    backgroundColor: '#7bb57f',
+    borderRadius: 10 * SCALE_HEIGHT,
+    paddingHorizontal: 8 * SCALE_WIDTH,
+    paddingVertical: 2 * SCALE_HEIGHT,
+  },
+  achivement: {
+    fontSize: FS(8),
+    color: '#ffffff',
+    fontWeight: 'bold',
   },
 });
 
