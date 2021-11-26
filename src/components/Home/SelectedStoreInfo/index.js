83fc46891d9f1419596657411adb20f47460df1e
Hyodori04 <x40807@gmail.com>
2021-11-27 06:59:51 +0900

    feat : Selected Store Review

--- src/components/Home/SelectedStoreInfo/index.js
+++ src/components/Home/SelectedStoreInfo/index.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import style from 'constants/styles';
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import RatingStar from 'components/RatingStar';
+import { Context as ReviewContext } from 'context/Review';
 
-const SelectedStoreInfo = ({ Name, Address, Rating }) => {
+import style from 'constants/styles';
+import Icon from 'widgets/Icon';
+import FS, { SCALE_HEIGHT, SCALE_WIDTH } from 'lib/utils/normalize';
+
+const SelectedStoreInfo = () => {
+  const { state } = useContext(ReviewContext);
+  const Reviewlen = state.reviewLists.length;
+  const { Rating, Information } = state.currentStore[0];
+  const { name, vicinity } = Information;
   return (
-    <View>
-      <View style={style.flexRow}>
-        <Text>{Name}</Text>
-        <Text> 별점{Rating}</Text>
+    <View style={style.flexRow}>
+      <View style={[styles.container]}>
+        <View style={style.flexRow}>
+          <Text style={styles.textname}>{name}</Text>
+          <RatingStar number={Rating} size={14} />
+        </View>
+        <View style={style.flexRow}>
+          <Icon source={require('public/icons/selectedstore_location.png')} style={styles.icon} />
+          <Text>{vicinity}</Text>
+        </View>
       </View>
-      <View>
-        <Text>{Address}</Text>
+      <View style={style.flexRow}>
+        <Text style={styles.textcategory}>
+          {' '}
+          별점<Text style={styles.textelement}>{`  ${Rating}`}</Text>
+        </Text>
+        <Text style={styles.textcategory}>
+          {'    '}
+          리뷰<Text style={styles.textelement}>{`  ${Reviewlen}`}</Text>
+        </Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 53 * SCALE_HEIGHT,
+    width: 250 * SCALE_WIDTH,
+    justifyContent: 'space-evenly',
+  },
+  textname: {
+    fontSize: FS(18),
+    fontWeight: 'bold',
+    marginRight: 7 * SCALE_WIDTH,
+  },
+  icon: {
+    width: 17 * SCALE_WIDTH,
+    height: 17 * SCALE_HEIGHT,
+  },
+  textaddress: {
+    fontSize: FS(12),
+    marginLeft: 2 * SCALE_WIDTH,
+  },
+  textcategory: {
+    fontSize: FS(12),
+    fontWeight: 'bold',
+  },
+  textelement: {
+    fontSize: FS(12),
+    fontWeight: 'normal',
+  },
+});
 
 export default SelectedStoreInfo;
--- src/templates/Main/Create/Review/SelectedStoreReview.js
+++ src/templates/Main/Create/Review/SelectedStoreReview.js
@@ -1,14 +1,18 @@
 import React, { useContext, useEffect } from 'react';
-import { ScrollView, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { Context as ReviewContext } from 'context/Review';
-import ReviewCard from 'components/ReviewCard';
+import CardView from 'components/CardView';
 import LoadingIndicator from 'components/LoadingIndicator';
 import SelectedStoreInfo from 'components/Home/SelectedStoreInfo';
+import Header from 'components/CardView/StoreReview/Header';
+import TitleHeader from 'components/Header';
+import Footer from 'components/CardView/SelectedList/Footer';
+import style from 'constants/styles';
+import FS, { SCALE_WIDTH, SCALE_HEIGHT } from 'lib/utils/normalize';
 import { push } from 'lib/utils/navigation';
 
 const SelectedStoreReview = ({ id }) => {
   const { state, getSelectedReview } = useContext(ReviewContext);
-
   const onClickPostReview = () => {
     push('CreateReview', { id });
   };
@@ -16,45 +20,66 @@ const SelectedStoreReview = ({ id }) => {
   useEffect(() => {
     getSelectedReview({ id });
   }, []);
-
   return (
-    <>
+    <View style={[style.backwhite]}>
+      <TitleHeader back title="리뷰" titleStyle={style.headertitle} />
       {state.reviewLists ? (
         <>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <SelectedStoreInfo Name="zzz" Address="zz" Rating="3" />
-            {state.reviewLists.map((review) => {
-              const { Content, Photo, PostUser } = review;
-              const information = {
-                content: Content,
-                photo: Photo,
-                postUser: PostUser,
-              };
-              return <ReviewCard key={review._id} information={information} />;
-            })}
+          <ScrollView
+            style={[styles.padding, styles.scrollview]}
+            showsVerticalScrollIndicator={false}
+          >
+            <SelectedStoreInfo />
+            <View style={styles.paddingtop}>
+              {state.reviewLists.map((review) => {
+                const { Content, Photo, PostUser } = review;
+                return (
+                  <CardView
+                    header={<Header PostUser={PostUser} />}
+                    footer={<Footer comment={Content} />}
+                    photo={Photo && Photo[0]}
+                  />
+                );
+              })}
+            </View>
           </ScrollView>
           <TouchableOpacity style={styles.review} onPress={onClickPostReview}>
-            <Text>리뷰 쓰기</Text>
+            <Text style={styles.textreview}>리뷰쓰기</Text>
           </TouchableOpacity>
         </>
       ) : (
         <LoadingIndicator />
       )}
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   review: {
-    width: 86,
-    height: 86,
-    borderRadius: 86,
-    borderWidth: 1,
+    width: 70 * SCALE_WIDTH,
+    height: 70 * SCALE_WIDTH,
+    borderRadius: 70 * SCALE_HEIGHT,
+    backgroundColor: '#7BB57F',
     position: 'absolute',
     justifyContent: 'center',
     alignItems: 'center',
-    right: 20,
-    bottom: 20,
+    right: 30 * SCALE_WIDTH,
+    bottom: 90 * SCALE_HEIGHT,
+    opacity: 0.9,
+  },
+  padding: {
+    paddingHorizontal: 14 * SCALE_WIDTH,
+  },
+  scrollview: {
+    paddingTop: 14 * SCALE_HEIGHT,
+  },
+  paddingtop: {
+    paddingTop: 21 * SCALE_HEIGHT,
+  },
+  textreview: {
+    fontSize: FS(14),
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
