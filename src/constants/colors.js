cf13a1b8dbf29c7cf59ecfc060f8e78750c7c0f1
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-26 15:19:46 +0900

    fix: Add colors, styles

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,3 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const MAIN_COLOR = '#FFE384';
 export const SUB_COLOR = '#7CB680';
+export const SUB_COLOR2 = '#7CB781';
+export const COLOR_1 = '#191919';
+export const COLOR_2 = '#292929';
