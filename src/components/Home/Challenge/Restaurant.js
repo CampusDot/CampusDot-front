c814afe0130117d6786e41a395d1af0f183d5dd2
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-26 15:32:00 +0900

    fix: change layout in Home Challenge

--- src/components/Home/Challenge/Restaurant.js
+++ src/components/Home/Challenge/Restaurant.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { push } from 'lib/utils/navigation';
 import storeImage from 'lib/utils/storeImage';
+import FS, { SCALE_HEIGHT, SCALE_WIDTH } from 'lib/utils/normalize';
 import State from './State';
 
 const Restaurant = ({ information }) => {
   const { storeInfo, storeCount, completeCount } = information;
   const { title, photo, _id: id } = storeInfo;
+
+  const onClickChallnege = () => {
+    push('SelectedList', { id });
+  };
+
   return (
-    <TouchableOpacity onPress={() => push('SelectedList', { id })}>
+    <TouchableOpacity onPress={onClickChallnege} style={styles.container}>
       <State progress={(completeCount / storeCount) * 100}>
         <View
           style={{
@@ -25,18 +31,22 @@ const Restaurant = ({ information }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginRight: 20 * SCALE_WIDTH,
+    marginTop: 14 * SCALE_HEIGHT,
+  },
   img: {
-    width: 103,
-    height: 103,
-    borderRadius: 103,
+    width: 103 * SCALE_WIDTH,
+    height: 103 * SCALE_WIDTH,
+    borderRadius: 103 * SCALE_WIDTH,
     position: 'absolute',
-    bottom: -51.5,
-    left: 1.5,
-    // marginRight: 8,
-    // marginLeft: 10,
+    bottom: -51.5 * SCALE_HEIGHT,
+    left: 4 * SCALE_WIDTH,
   },
   text: {
     textAlign: 'center',
+    marginTop: 10 * SCALE_HEIGHT,
+    fontSize: FS(14),
   },
 });
 
--- src/components/Home/Challenge/index.js
+++ src/components/Home/Challenge/index.js
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, StyleSheet } from 'react-native';
 import { Context as StoreListContext } from 'context/StoreList';
 import LoadingIndicator from 'components/LoadingIndicator';
+import FS, { SCALE_HEIGHT, SCALE_WIDTH } from 'lib/utils/normalize';
 import Restaurant from './Restaurant';
 
 const Challenge = () => {
   const { state } = useContext(StoreListContext);
   return (
-    <View>
-      <Text style={{ marginTop: 100 }}>도전 중</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>도전 중</Text>
       {state.challengeStoreList ? (
         <FlatList
           horizontal
           data={state.challengeStoreList}
           keyExtractor={(store) => store.storeInfo._id}
           showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.flatContainer}
           renderItem={({ item }) => {
             return <Restaurant information={item} />;
           }}
@@ -25,5 +27,20 @@ const Challenge = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 13 * SCALE_HEIGHT,
+    marginBottom: 8 * SCALE_HEIGHT,
+  },
+  title: {
+    fontSize: FS(16),
+    fontWeight: 'bold',
+    marginLeft: 14 * SCALE_WIDTH,
+  },
+  flatContainer: {
+    paddingLeft: 14 * SCALE_WIDTH,
+  },
+});
 
 export default Challenge;
