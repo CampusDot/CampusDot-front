c3075afa10ba30f3006ed450cd3b88b299c8c575
Hyodori04 <x40807@gmail.com>
2021-11-26 18:34:50 +0900

    feat: review create and restaurant card view

--- src/components/RestaurantCardView/Footer.js
+++ src/components/RestaurantCardView/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import style from 'constants/styles';
+import Icon from 'widgets/Icon';
+import FS, { SCALE_WIDTH, SCALE_HEIGHT } from 'lib/utils/normalize';
 
-const Footer = ({ Content }) => {
+const Footer = ({ name, address }) => {
   return (
     <View style={[styles.container, style.flexRow, style.space_between]}>
-      <Text>{Content}</Text>
+      <View style={styles.info}>
+        <Text style={styles.textname}>{name}</Text>
+        <View style={[style.flexRow, styles.location]}>
+          <Icon source={require('public/icons/location.png')} style={styles.icon} />
+          <Text>{address}</Text>
+        </View>
+      </View>
+      <Icon source={require('public/icons/review_write.png')} style={styles.reviewicon} />
       <TouchableOpacity style={styles.icon} />
     </View>
   );
@@ -13,19 +22,35 @@ const Footer = ({ Content }) => {
 
 const styles = StyleSheet.create({
   container: {
-    height: 59,
-    borderWidth: 1,
+    height: 59 * SCALE_HEIGHT,
+    paddingHorizontal: 12 * SCALE_WIDTH,
+    paddingTop: 9 * SCALE_HEIGHT,
+    paddingBottom: 11 * SCALE_HEIGHT,
   },
   profileImg: {
-    width: 24,
-    height: 24,
-    borderRadius: 24,
+    width: 24 * SCALE_WIDTH,
+    height: 24 * SCALE_WIDTH,
+    borderRadius: 24 * SCALE_HEIGHT,
     borderWidth: 1,
   },
+  info: {
+    width: 250 * SCALE_WIDTH,
+  },
+  location: {
+    marginTop: 5 * SCALE_HEIGHT,
+  },
   icon: {
-    width: 40,
-    height: 40,
-    borderWidth: 1,
+    width: 16 * SCALE_WIDTH,
+    height: 16 * SCALE_WIDTH,
+  },
+  textname: {
+    fontSize: FS(14),
+    fontWeight: 'bold',
+  },
+  reviewicon: {
+    width: 72 * SCALE_WIDTH,
+    height: 25 * SCALE_HEIGHT,
+    backgroundColor: 'white',
   },
 });
 
--- src/components/RestaurantCardView/Header.js
+++ src/components/RestaurantCardView/Header.js
@@ -2,27 +2,48 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import style from 'constants/styles';
 import { MAIN_COLOR } from 'constants/colors';
+import FS, { SCALE_WIDTH, SCALE_HEIGHT } from 'lib/utils/normalize';
+import Rating from 'components/Rating';
 
-const Header = ({ name, address }) => {
+const Header = ({ rating, review }) => {
+  const reviewcount = review.length;
+  const temprating = 4;
+  const tempreviewcount = 16;
   return (
     <View style={[styles.container, style.flexRow]}>
-      <Text>{name}</Text>
-      <Text>{address}</Text>
+      <Text style={styles.textcategory}>리뷰</Text>
+      <Text style={[styles.textelement, styles.elementmargin]}>{tempreviewcount}</Text>
+      <Text style={[styles.textcategory, styles.categorymargin]}>별점</Text>
+      <Text style={[styles.textelement, styles.elementmargin]}>{temprating}</Text>
+      <Rating number={temprating} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 40,
-    borderWidth: 1,
-    backgroundColor: MAIN_COLOR,
+    height: 40 * SCALE_HEIGHT,
+    paddingHorizontal: 10 * SCALE_WIDTH,
   },
   profileImg: {
     width: 24,
     height: 24,
     borderRadius: 24,
     borderWidth: 1,
+  },
+  textcategory: {
+    fontSize: FS(12),
+    fontWeight: 'bold',
+  },
+  textelement: {
+    fontSize: FS(12),
+  },
+  elementmargin: {
+    marginLeft: 6 * SCALE_WIDTH,
+    marginRight: 6 * SCALE_WIDTH,
+  },
+  categorymargin: {
+    marginLeft: 10 * SCALE_WIDTH,
   },
 });
 
