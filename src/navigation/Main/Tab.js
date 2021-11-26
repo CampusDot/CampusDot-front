0bdfd223bce08c7374fb3cb93185abef16b6e691
Hyodori04 <x40807@gmail.com>
2021-11-26 12:15:27 +0900

    fix : Tab Setting

--- src/navigation/Main/Tab.js
+++ src/navigation/Main/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from 'screens/Main/Home';
 import Stamp from 'screens/Main/Stamp';
@@ -10,15 +11,35 @@ const MyModalBackgroundScreen = () => null;
 
 const TabScreen = () => (
   <Tab.Navigator
+    tabBarOptions={{
+      showLabel: false,
+    }}
     screenOptions={{
       headerShown: false,
     }}
   >
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Stamp" component={Stamp} />
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ focused }) => (focused ? <Text>a</Text> : <Text>b</Text>),
+      }}
+    />
+    <Tab.Screen
+      name="Stamp"
+      component={Stamp}
+      options={{
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ focused }) => (focused ? <Text>a</Text> : <Text>b</Text>),
+      }}
+    />
     <Tab.Screen
       name="Create"
       component={MyModalBackgroundScreen}
+      options={{
+        tabBarIcon: () => <Text>c</Text>,
+      }}
       listeners={({ navigation }) => ({
         tabPress: (e) => {
           e.preventDefault();
@@ -26,8 +47,22 @@ const TabScreen = () => (
         },
       })}
     />
-    <Tab.Screen name="Notice" component={Notice} />
-    <Tab.Screen name="MyPage" component={MyPage} />
+    <Tab.Screen
+      name="Notice"
+      component={Notice}
+      options={{
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ focused }) => (focused ? <Text>a</Text> : <Text>b</Text>),
+      }}
+    />
+    <Tab.Screen
+      name="MyPage"
+      component={MyPage}
+      options={{
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({ focused }) => (focused ? <Text>a</Text> : <Text>b</Text>),
+      }}
+    />
   </Tab.Navigator>
 );
 
