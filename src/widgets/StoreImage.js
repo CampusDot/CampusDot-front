87e68bbb3f02c024dfb7e75e591ec607e61ffcd7
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-12-02 02:06:23 +0900

    fix: change StoreImage widgets

--- src/widgets/StoreImage.js
+++ src/widgets/StoreImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GOOGLE_MAPS_KEY } from 'constants/app';
-import { Image, View } from 'react-native';
+import { Image } from 'react-native';
+import isCustomImage from 'lib/utils/customImage';
 
 const urlConverter = (reference) => {
   return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${reference}&key=${GOOGLE_MAPS_KEY}`;
@@ -8,13 +9,10 @@ const urlConverter = (reference) => {
 
 const StoreImage = ({ image, imageStyle }) => {
   return (
-    <>
-      {image !== '' ? (
-        <Image style={imageStyle} source={{ uri: urlConverter(image) }} />
-      ) : (
-        <View style={imageStyle} />
-      )}
-    </>
+    <Image
+      style={imageStyle}
+      source={{ uri: isCustomImage(image) ? image : urlConverter(image) }}
+    />
   );
 };
 
