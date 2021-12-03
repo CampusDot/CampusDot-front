8af600e355266c46dccc2f712e16c0cc833d147d
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-12-04 00:17:09 +0900

    fix: change colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export const MAIN_COLOR = '#FFE384';
-export const SUB_COLOR = '#7CB680';
-export const SUB_COLOR2 = '#7CB781';
+export const MAIN_COLOR = '#FFDE6E';
+export const SUB_COLOR = '#73B979';
+export const SUB_COLOR2 = '#5FAF66';
 export const COLOR_1 = '#191919';
 export const COLOR_2 = '#292929';
